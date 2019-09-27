b2cb38b2910232ca4662a4c5b27b3a49dc46d9ab
markjonessolano <mjsol12>
2019-09-28 01:41:25 +0800

    data binding parents-child-parents

--- src/component/listHeader.js
+++ src/component/listHeader.js
@@ -3,9 +3,9 @@ import React, {Component} from 'react';
 class Header extends Component {
   render () {
     return (
-      <Header style={headerStyle}>
+      <div style={headerStyle}>
         <h1>Todo List</h1>
-      </Header>
+      </div>
     )
   }
 }

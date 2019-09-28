f7a9692c7bd6850c3cac5e74db9b4e87ed21807e
markjonessolano <mjsol12>
2019-09-28 23:51:25 +0800

    Updates

---
+++ precache-manifest.1e193fde69709261a6e6bf24da8a26db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391c01b1d55897863a878ff80c72ff90",
+    "url": "/React-test/index.html"
+  },
+  {
+    "revision": "2a08a46478e130a81d3d",
+    "url": "/React-test/static/css/main.cbac02b2.chunk.css"
+  },
+  {
+    "revision": "ee1d0078a1cca6031534",
+    "url": "/React-test/static/js/2.72b02bef.chunk.js"
+  },
+  {
+    "revision": "2a08a46478e130a81d3d",
+    "url": "/React-test/static/js/main.b0136b73.chunk.js"
+  },
+  {
+    "revision": "4b93414332fe8be50fef",
+    "url": "/React-test/static/js/runtime-main.e45f62b2.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-test/precache-manifest.1e193fde69709261a6e6bf24da8a26db.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
